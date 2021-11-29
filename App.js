32be0098029cca55429f6c86a50131103cc465f6
Frank W <super.fairy.dev@gmail.com>
2021-11-30 01:00:39 +0300

    feat: connected redux to Home screen

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { HomeScreen } from "./src/containers";
+import { customConfigureStore } from "./src/redux";
 
 export default function App() {
-  return <HomeScreen />;
+  const { Provider, store } = customConfigureStore();
+  return (
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
+  );
 }
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native";
 import { styles } from "./styles";
 import { NameInput, NameList } from "../../components";
 import { NameData } from "./dummy";
+import { withHome } from "../../redux/containers";
 
-const HomeScreen = () => {
-  const onAddName = (name) => alert(name);
+const HomeScreen = (props) => {
+  const onAddName = (name) => {
+    const data = {
+      id: new Date().getTime(),
+      name,
+      date: new Date(),
+    };
+    props.addName(data);
+  };
+
+  useEffect(() => {
+    props.fetchNameListData();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
       <NameInput onAdd={onAddName} />
-      <NameList data={NameData} />
+      <NameList data={props.nameListData} />
     </SafeAreaView>
   );
 };
 
-export default HomeScreen;
+export default withHome(HomeScreen);
