a544a3b209a30b0e7bb4319af64a339e813a86a6
Frank W <super.fairy.dev@gmail.com>
2021-11-30 00:55:02 +0300

    fix: date string format

--- src/containers/Home/dummy.js
+++ src/containers/Home/dummy.js
@@ -4,7 +4,7 @@ const count = 20;
 const NameData = _.map(_.range(0, count), (value, index) => {
   const id = index;
   const name = `Test ${index}`;
-  const date = new Date().toLocaleString();
+  const date = new Date().toString();
   return {
     id,
     name,
